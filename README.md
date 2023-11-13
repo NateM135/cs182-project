bde5eff2ba441e61807d6cac471305346c5b5759
TuanLenh <tuanlenh3085@gmail.com>
2023-11-12 17:42:47 -0800

    set up repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# 182 project
+
+## Setup + Compile + Run Checker using libxml
+
+```
+sudo apt install libxml2-dev
+gcc validate.c -I/usr/include/libxml2 -lxml2
+./a.out
+```
+
+## Setup + Run Python Script
+
+```
+python3 -m venv venv/
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
+python3 script.py
+```
---
+++ validate.c
@@ -0,0 +1,86 @@
+/**
+ * section: xmlReader
+ * synopsis: Parse and validate an XML file with an xmlReader
+ * purpose: Demonstrate the use of xmlReaderForFile() to parse an XML file
+ *          validating the content in the process and activating options
+ *          like entities substitution, and DTD attributes defaulting.
+ *          (Note that the XMLReader functions require libxml2 version later
+ *          than 2.6.)
+ * usage: reader2 <valid_xml_filename>
+ * test: reader2 test2.xml > reader1.tmp && diff reader1.tmp $(srcdir)/reader1.res
+ * author: Daniel Veillard
+ * copy: see Copyright for the status of this software.
+ */
+
+ // modified version of this dudes code
+
+#include <stdio.h>
+#include <libxml/xmlreader.h>
+#include <libxml/parser.h>
+
+#ifdef LIBXML_READER_ENABLED
+
+/**
+ * processNode:
+ * @reader: the xmlReader
+ *
+ * Dump information about the current node
+ */
+static void
+processNode(xmlTextReaderPtr reader) {
+    const xmlChar *name, *value;
+
+    name = xmlTextReaderConstName(reader);
+    if (name == NULL)
+	name = BAD_CAST "--";
+
+    value = xmlTextReaderConstValue(reader);
+}
+
+/**
+ * streamFile:
+ * @filename: the file name to parse
+ *
+ * Parse, validate and print information about an XML file.
+ */
+static void
+streamFile(const char *filename) {
+    xmlTextReaderPtr reader;
+    int ret;
+
+    reader = xmlReaderForFile(filename, NULL, 0);
+    if (reader != NULL) {
+        ret = xmlTextReaderRead(reader);
+        while (ret == 1) {
+            processNode(reader);
+            ret = xmlTextReaderRead(reader);
+        }
+        xmlFreeTextReader(reader);
+        if (ret != 0) {
+            fprintf(stderr, "%s : failed to parse\n", filename);
+        }
+        else {
+            printf("Successfully Validated XML Syntax!\n");
+        }
+    } else {
+        fprintf(stderr, "Unable to open %s\n", filename);
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2)
+        return(1);
+
+    LIBXML_TEST_VERSION
+
+    streamFile(argv[1]);
+
+    return(0);
+}
+
+#else
+int main(void) {
+    fprintf(stderr, "XInclude support not compiled in\n");
+    return(0);
+}
+#endif
