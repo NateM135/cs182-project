3ddde756a5d04f22cc2e09375c1e7a254bcd3b46
Nathan Melwani <melwaninate@gmail.com>
2023-11-14 00:04:54 +0000

    run multiple times + validate xml

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
+import os
+
 from transformers import pipeline, set_seed
 
-generator = pipeline('text-generation', model='gpt2')
+OUTPUT_FILENAME = "generated_text.xml"
+SEED = 42
+MAX_RESPONSE_LENGTH = 100
+NUM_ITERATIONS = 50
+VALIDATION_BINARY_NAME = "a.out"
 
-set_seed(42)
-
-prompt_template = """
+SAMPLE_PROMPT = """
 <doc>
     <clean> </clean>
     <dirty> A B </dirty>
@@ -18,9 +22,18 @@ prompt_template = """
 </doc>
 """
 
-x = generator(prompt_template, max_length=100, num_return_sequences=5)
-for i, response in enumerate(x):
-    with open(f'generated_xml_{i+1}.xml', 'w') as file:
-        file.write(response['generated_text'])
+set_seed(SEED)
+generator = pipeline('text-generation', model='gpt2')
+
+num_successful = 0
+for _ in range(NUM_ITERATIONS):
+    x = generator(SAMPLE_PROMPT, max_length=105, num_return_sequences=1)
+    for i, response in enumerate(x):
+        with open(OUTPUT_FILENAME, "w") as file:
+            file.write(response["generated_text"])
+        status = os.popen(f"./{VALIDATION_BINARY_NAME} {OUTPUT_FILENAME}").read()
+        if status.strip() == "Successfully Validated XML Syntax!":
+            num_successful+=1
 
 print("Generated XML files successfully.")
+print(f"{num_successful}/{NUM_ITERATIONS} Iterations Successful")
