e3a9c4a9f9ad22d86372c45ff01f390c0c510f30
zackwcs <zackwong02@gmail.com>
2023-11-29 16:28:28 -0800

    New updated prompts

--- prompts.py
+++ prompts.py
@@ -1,18 +1,7 @@
 PROMPTS = {
 
-"Ask For Example":"""
-Write a well-formed XML document that represents information about a book. The XML should include the following elements:
-
-<book> - representing the entire book.
-   - <title> - the title of the book.
-   - <author> - the author of the book.
-   - <published_year> - the year the book was published.
-
-Here is an example output:
-
-<book><title>The Adventures of GPT-2</title> <author>AI Writer</author> <published_year>2023</published_year> </book>
-
-Another example of an XML like this where the elements are properly nested, and include sample data within each element is:
+"PROMPT ONE":"""
+An example of a valid XML on a single line is:
 """,
 
 "PROMPT TWO":"""
@@ -33,11 +22,6 @@ Another example of an XML like this where the elements are properly nested, and 
 "PROMPT THREE":"""
 Write a well-formed XML document that represents information about a book. The XML should include the following elements:
 
-<book> - representing the entire book.
-   - <title> - the title of the book.
-   - <author> - the author of the book.
-   - <published_year> - the year the book was published.
-
 Here is an example output:
 
 <book><title>The Adventures of GPT-2</title> <author>AI Writer</author> <published_year>2023</published_year> </book>
@@ -46,31 +30,27 @@ Another example of an XML like this where the elements are properly nested, and 
 """,
 
 "PROMPT FOUR":"""
-Write a well-formed XML document that represents information about a book. The XML should include the following elements:
+Write a well-formed XML document that represents information about a schedule. Each schedule contains a lecture element withe following elements.
 
-<book> - representing the entire book.
-   - <title> - the title of the book.
-   - <author> - the author of the book.
-   - <published_year> - the year the book was published.
+<schedule> - representing the entire schedule.
+   - <lecture> - the title of the lecture.
+   - <topic> - the topic of the lecture
+   - <instructor> - the name of the instructor
+   - <time> - the week # for when this lecture takes place
 
 Here is an example output:
 
-<book><title>The Adventures of GPT-2</title> <author>AI Writer</author> <published_year>2023</published_year> </book>
+<schedule><lecture><topic>Testing Overview</topic><instructor>Qian Zhang</instructor><time>Week 1</time></lecture><lecture><topic>Input Space Partitioning</topic><instructor>Qian Zhang</instructor><time>Week 2</time></lecture><lecture><topic>Test Coverage</topic><instructor>Qian Zhang</instructor><time>Week 3</time></lecture></schedule>
 
 Another example of an XML like this where the elements are properly nested, and include sample data within each element is:
 """,
 
 "PROMPT FIVE":"""
-Write a well-formed XML document that represents information about a book. The XML should include the following elements:
-
-<book> - representing the entire book.
-   - <title> - the title of the book.
-   - <author> - the author of the book.
-   - <published_year> - the year the book was published.
+Write a well-formed XML document that represents information about a schedule. 
 
 Here is an example output:
 
-<book><title>The Adventures of GPT-2</title> <author>AI Writer</author> <published_year>2023</published_year> </book>
+<schedule><lecture><topic>Testing Overview</topic><instructor>Qian Zhang</instructor><time>Week 1</time></lecture><lecture><topic>Input Space Partitioning</topic><instructor>Qian Zhang</instructor><time>Week 2</time></lecture><lecture><topic>Test Coverage</topic><instructor>Qian Zhang</instructor><time>Week 3</time></lecture></schedule>
 
 Another example of an XML like this where the elements are properly nested, and include sample data within each element is:
 """
